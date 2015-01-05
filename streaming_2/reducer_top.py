c006363d7d459b41463ef681b40234bf05550019
Stuart Wakefield <me@stuartwakefield.co.uk>
2015-01-05 21:17:21 +0000

    Add top reducer

---
+++ streaming_2/reducer_top.py
@@ -0,0 +1,27 @@
+#!/bin/env python
+import sys
+
+def reducer():
+
+    result = 0
+    last = None
+
+    for line in sys.stdin:
+        data = line.strip().split("\t")
+
+        if len(data) != 2:
+            continue
+
+        key, value = data
+
+        count = int(value)
+
+        if count > result:
+            result = count
+            last = key
+
+    if last:
+        print("%s\t%s" % (last, count))
+
+if __name__=='__main__':
+    reducer()
