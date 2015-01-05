ee9df4a7e9c40d369846c1f96277f3e9ff1e3a5c
Stuart Wakefield <me@stuartwakefield.co.uk>
2015-01-05 20:59:51 +0000

    Add path sanitiser mapper

---
+++ streaming_2/mapper_path_sanitised_log.py
@@ -0,0 +1,31 @@
+#!/bin/env python
+import sys
+import re
+
+log_regexp = r"^(\S+)\s(\S+)\s(\S+)\s\[(\S+\s\S+)\]\s\"([^\"]+)\"\s(\S+)\s(\S+)$"
+
+def mapper():
+    for line in sys.stdin:
+        try:
+            matches = re.match(log_regexp, line.strip())
+            
+            if matches and len(matches.groups()) != 7:
+                continue
+
+            ip, identity, username, time, request, status, bytes = matches.groups()
+            
+            matches = re.match(r"^(\S+)\s(\S+)\s(\S+)$", request)
+            
+            if matches and len(matches.groups()) != 3:
+                continue
+
+            method, path, protocol = matches.groups()
+
+            sanitised = re.sub(r"^http\:\/\/[^\/]+", "", path)
+            
+            print("%s\t%s" % (sanitised, line.strip()))
+        except:
+            continue
+
+if __name__=='__main__':
+    mapper()
